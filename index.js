d9b424bc9a7e86ec36b2250ba0354cff761fb67c
boniaminshawon <shawonboni29@gmail.com>
2024-06-05 09:01:29 +0600

    made jwt and middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -29,6 +30,29 @@ async function run() {
         const reviewCollection = client.db('AItech').collection('reviews');
         const blogCollection = client.db('AItech').collection('blogs');
 
+        // jwt related api 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '360d' });
+            res.send({ token });
+        })
+
+          // middleware
+          const verifyToken = (req, res, next) => {
+            // console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+
+        }
 
         // service related api
         app.get('/services', async (req, res) => {
